2e249726319524521e22cb11cc294860a95265dd
Евгений Ильиных <gmg1vh@gmail.com>
2016-07-16 00:28:25 +0500

    Initial

---
+++ To-Do/MainForm.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace To_Do
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.ToDoList = new System.Windows.Forms.DataGridView();
+            this.modifyButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.filterButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.repeatBox = new System.Windows.Forms.ComboBox();
+            this.deadlineCalendar = new System.Windows.Forms.MonthCalendar();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.addBox = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.showAllButton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.fromTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.toTextBox = new System.Windows.Forms.MaskedTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ToDoList)).BeginInit();
+            this.addBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ToDoList
+            // 
+            this.ToDoList.AllowUserToAddRows = false;
+            this.ToDoList.AllowUserToDeleteRows = false;
+            this.ToDoList.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.ToDoList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ToDoList.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.ToDoList.Location = new System.Drawing.Point(113, 25);
+            this.ToDoList.MultiSelect = false;
+            this.ToDoList.Name = "ToDoList";
+            this.ToDoList.ReadOnly = true;
+            this.ToDoList.RowHeadersVisible = false;
+            this.ToDoList.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.ToDoList.Size = new System.Drawing.Size(358, 236);
+            this.ToDoList.TabIndex = 0;
+            // 
+            // modifyButton
+            // 
+            this.modifyButton.Location = new System.Drawing.Point(12, 42);
+            this.modifyButton.Name = "modifyButton";
+            this.modifyButton.Size = new System.Drawing.Size(91, 23);
+            this.modifyButton.TabIndex = 2;
+            this.modifyButton.Text = "Изменить";
+            this.modifyButton.UseVisualStyleBackColor = true;
+            this.modifyButton.Click += new System.EventHandler(this.modifyButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(12, 71);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(91, 23);
+            this.deleteButton.TabIndex = 3;
+            this.deleteButton.Text = "Удалить";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // filterButton
+            // 
+            this.filterButton.Location = new System.Drawing.Point(12, 193);
+            this.filterButton.Name = "filterButton";
+            this.filterButton.Size = new System.Drawing.Size(91, 23);
+            this.filterButton.TabIndex = 4;
+            this.filterButton.Text = "Фильтр";
+            this.filterButton.UseVisualStyleBackColor = true;
+            this.filterButton.Click += new System.EventHandler(this.filterButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(86, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Периодичность";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(295, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Крайний срок";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(88, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Наименование";
+            // 
+            // repeatBox
+            // 
+            this.repeatBox.FormattingEnabled = true;
+            this.repeatBox.Items.AddRange(new object[] {
+            "Каждый день",
+            "Каждую неделю",
+            "Каждый месяц"});
+            this.repeatBox.Location = new System.Drawing.Point(66, 123);
+            this.repeatBox.MaxLength = 255;
+            this.repeatBox.Name = "repeatBox";
+            this.repeatBox.Size = new System.Drawing.Size(121, 21);
+            this.repeatBox.TabIndex = 12;
+            // 
+            // deadlineCalendar
+            // 
+            this.deadlineCalendar.Location = new System.Drawing.Point(253, 72);
+            this.deadlineCalendar.MaxSelectionCount = 1;
+            this.deadlineCalendar.Name = "deadlineCalendar";
+            this.deadlineCalendar.TabIndex = 11;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(11, 72);
+            this.nameTextBox.MaxLength = 255;
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(230, 20);
+            this.nameTextBox.TabIndex = 10;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(89, 194);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 8;
+            this.addButton.Text = "Добавить";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // addBox
+            // 
+            this.addBox.Controls.Add(this.nameTextBox);
+            this.addBox.Controls.Add(this.addButton);
+            this.addBox.Controls.Add(this.deadlineCalendar);
+            this.addBox.Controls.Add(this.label2);
+            this.addBox.Controls.Add(this.label3);
+            this.addBox.Controls.Add(this.repeatBox);
+            this.addBox.Controls.Add(this.label1);
+            this.addBox.Location = new System.Drawing.Point(477, -1);
+            this.addBox.Name = "addBox";
+            this.addBox.Size = new System.Drawing.Size(429, 262);
+            this.addBox.TabIndex = 18;
+            this.addBox.TabStop = false;
+            this.addBox.Text = "Добавление дела";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(245, 6);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 16);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Список дел";
+            // 
+            // showAllButton
+            // 
+            this.showAllButton.Location = new System.Drawing.Point(12, 226);
+            this.showAllButton.Name = "showAllButton";
+            this.showAllButton.Size = new System.Drawing.Size(91, 23);
+            this.showAllButton.TabIndex = 19;
+            this.showAllButton.Text = "Показать все";
+            this.showAllButton.UseVisualStyleBackColor = true;
+            this.showAllButton.Click += new System.EventHandler(this.showAllButton_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 120);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Начальная дата";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(15, 154);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 13);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Конечная дата";
+            // 
+            // fromTextBox
+            // 
+            this.fromTextBox.Location = new System.Drawing.Point(21, 133);
+            this.fromTextBox.Mask = "00.00.0000";
+            this.fromTextBox.Name = "fromTextBox";
+            this.fromTextBox.Size = new System.Drawing.Size(75, 20);
+            this.fromTextBox.TabIndex = 21;
+            // 
+            // toTextBox
+            // 
+            this.toTextBox.Location = new System.Drawing.Point(21, 167);
+            this.toTextBox.Mask = "00.00.0000";
+            this.toTextBox.Name = "toTextBox";
+            this.toTextBox.Size = new System.Drawing.Size(75, 20);
+            this.toTextBox.TabIndex = 22;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(907, 261);
+            this.Controls.Add(this.toTextBox);
+            this.Controls.Add(this.fromTextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.showAllButton);
+            this.Controls.Add(this.addBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.filterButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.modifyButton);
+            this.Controls.Add(this.ToDoList);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Список дел";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ToDoList)).EndInit();
+            this.addBox.ResumeLayout(false);
+            this.addBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView ToDoList;
+        private System.Windows.Forms.Button modifyButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button filterButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox repeatBox;
+        private System.Windows.Forms.MonthCalendar deadlineCalendar;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.GroupBox addBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button showAllButton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.MaskedTextBox fromTextBox;
+        private System.Windows.Forms.MaskedTextBox toTextBox;
+    }
+}
+
---
+++ To-Do/MainForm.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlServerCe;
+using System.IO;
+using System.Windows.Forms;
+
+namespace To_Do
+{
+    public partial class MainForm : Form
+    {
+        private const string ConnectionString = "Data Source=|DataDirectory|\\db.sdf";
+        private SqlCeDataAdapter _dataAdapter;
+        private SqlCeCommandBuilder _commandBuilder;
+        private BindingSource _bindingSource;
+        private DataTable _dataTable;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            if (!File.Exists("db.sdf"))
+            {
+                MessageBox.Show("База данных не обнаружена! Приложение будет закрыто.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+            RefreshList();
+        }
+
+        private void FillTable(string selectCommand)
+        {
+            try
+            {
+                _dataAdapter = new SqlCeDataAdapter(selectCommand, ConnectionString);
+                _commandBuilder = new SqlCeCommandBuilder(_dataAdapter);
+                _dataTable = new DataTable();
+                _dataAdapter.Fill(_dataTable);
+                _bindingSource = new BindingSource {DataSource = _dataTable};
+            }
+            catch (SqlCeException ex)
+            {
+                ErrorLog(ex);
+            }
+            catch (InvalidOperationException ex)
+            {
+                ErrorLog(ex);
+            }
+        }
+
+        private void RefreshList()
+        {
+            FillTable("SELECT * FROM List");
+            ToDoList.DataSource = _bindingSource;
+            ToDoList.Columns[0].Visible = false;
+            ToDoList.Columns[1].HeaderText = "Наименование";
+            ToDoList.Columns[2].HeaderText = "Крайний срок";
+            ToDoList.Columns[3].HeaderText = "Периодичность";
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            if (ToDoList.SelectedRows.Count == 0) return;
+            string cell1 = ToDoList.SelectedRows[0].Cells[1].Value.ToString();
+            string cell3 = ToDoList.SelectedRows[0].Cells[3].Value.ToString();
+
+            for (int i = ToDoList.Rows.Count - 1; i > -1; i--)
+            {
+                DataGridViewRow row = ToDoList.Rows[i];
+                if (cell1 == row.Cells[1].Value.ToString() &
+                    cell3 == row.Cells[3].Value.ToString())
+                {
+                    try
+                    {
+                        ToDoList.Rows.Remove(row);
+                    }
+                    catch (Exception ex)
+                    {
+                        ErrorLog(ex);
+                    }
+
+                }
+            }
+            _dataAdapter.Update((DataTable) _bindingSource.DataSource);
+        }
+
+        private void filterButton_Click(object sender, EventArgs e)
+        {
+            if (fromTextBox.Text == "" || toTextBox.Text == "") return;
+            try
+            {
+                string fromDate = Convert.ToDateTime(fromTextBox.Text).ToString("yyyyMMdd");
+                string toDate = Convert.ToDateTime(toTextBox.Text).ToString("yyyyMMdd");
+                FillTable("SELECT * FROM List WHERE Deadline BETWEEN '" + fromDate + "' AND '" + toDate + "'");
+                ToDoList.DataSource = _bindingSource;
+            }
+            catch (FormatException ex)
+            {
+                ErrorLog(ex);
+            }
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            if (nameTextBox.Text != "")
+            {
+                
+                DateTime deadline = Convert.ToDateTime(deadlineCalendar.SelectionRange.Start.ToShortDateString());
+                int daysLeft = DateTime.DaysInMonth(deadlineCalendar.SelectionRange.Start.Year, deadlineCalendar.SelectionRange.Start.Month) - deadlineCalendar.SelectionRange.Start.Day;
+                int monthsLeft = 12 - deadlineCalendar.SelectionRange.Start.Month;
+                switch (repeatBox.Text)
+                {
+
+                    case "Каждый день":
+
+                        for (int i = 0; i < daysLeft+1; i++)
+                        {
+                            deadline = Convert.ToDateTime(deadlineCalendar.SelectionRange.Start.ToShortDateString()).AddDays(i);
+                            AddToDb(deadline);
+                        }
+                        break;
+                    case "Каждую неделю":
+
+                        for (int i = 0; i < daysLeft+1;i++)
+                        {
+                            deadline = Convert.ToDateTime(deadlineCalendar.SelectionRange.Start.ToShortDateString()).AddDays(i);
+                            AddToDb(deadline);
+                            i = i + 6;
+                        }
+                        break;
+                    case "Каждый месяц":
+                        for (int i = 0; i < monthsLeft+1; i++)
+                        {
+                            deadline = Convert.ToDateTime(deadlineCalendar.SelectionRange.Start.ToShortDateString()).AddMonths(i);
+                            AddToDb(deadline);
+                        }
+                        break;
+                    default:
+                        AddToDb(deadline);
+                        break;
+                }
+                nameTextBox.Text = "";
+                deadlineCalendar.SetDate(DateTime.Now);
+                repeatBox.Text = "";
+                addBox.Text = "Добавление дела";
+                addButton.Text = "Добавить";
+            }
+            else
+                MessageBox.Show("Заполните поле 'Наименование'", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+
+            RefreshList();
+        }
+
+        private void AddToDb(DateTime deadline)
+        {
+            try
+            {
+                using (SqlCeConnection connection = new SqlCeConnection(ConnectionString))
+                {
+                    connection.Open();
+                    string sqlquery = ("INSERT INTO List ([Name],[Deadline],[Repeat]) " +
+                                       "VALUES (@name, @deadline, @repeat)");
+                    SqlCeCommand cmd = new SqlCeCommand(sqlquery, connection);
+                    cmd.Parameters.AddWithValue("@name", nameTextBox.Text);
+                    cmd.Parameters.AddWithValue("@deadline", deadline);
+                    cmd.Parameters.AddWithValue("@repeat", repeatBox.Text);
+                    cmd.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorLog(ex);
+            }
+        }
+
+        private void modifyButton_Click(object sender, EventArgs e)
+        {
+
+            if (ToDoList.SelectedRows.Count == 0) return;
+            addBox.Text = "Изменение дела";
+            addButton.Text = "Изменить";
+
+            nameTextBox.Text = ToDoList.SelectedRows[0].Cells[1].Value.ToString();
+            deadlineCalendar.SetSelectionRange(Convert.ToDateTime(ToDoList.SelectedRows[0].Cells[2].Value.ToString()),
+                Convert.ToDateTime(ToDoList.SelectedRows[0].Cells[2].Value.ToString()));
+            repeatBox.Text = ToDoList.SelectedRows[0].Cells[3].Value.ToString();
+
+            for (int i = ToDoList.Rows.Count-1; i >-1; i--)
+            {
+                DataGridViewRow row = ToDoList.Rows[i];
+                if (nameTextBox.Text == row.Cells[1].Value.ToString() &
+                    repeatBox.Text == row.Cells[3].Value.ToString())
+                {
+                    ToDoList.Rows.Remove(row);
+                }
+            }
+
+            _dataAdapter.Update((DataTable)_bindingSource.DataSource);
+        }
+
+        private void showAllButton_Click(object sender, EventArgs e)
+        {
+            RefreshList();
+            fromTextBox.Clear();
+            toTextBox.Clear();
+        }
+
+        private static void ErrorLog(Exception ex)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter("Error.log", true))
+                {
+                    writer.WriteLine("Time: " + DateTime.Now);
+                    writer.WriteLine("Message: " + ex.Message);
+                    writer.WriteLine("Trace: " + ex.StackTrace);
+                    writer.WriteLine("-----------------------------------------------------------------------------");
+                }
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+        }
+    }
+}
